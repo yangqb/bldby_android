78b00d1f41b8c600d2b5afb9570c8517027978ea
bch <15011462696@163.com>
2020-06-22 18:20:58 +0800

    网络修改,商品详情修改

---
+++ baselibrary/src/main/java/com/bldby/baselibrary/app/network/BaseAir1Response.java
@@ -0,0 +1,41 @@
+package com.bldby.baselibrary.app.network;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.bldby.baselibrary.core.network.BaseApiResponse;
+
+/**
+ * package name: com.bldby.baselibrary.app.network
+ * user: yangqinbo
+ * date: 2020/6/17
+ * time: 20:38
+ * email: 694125155@qq.com
+ */
+public class BaseAir1Response extends BaseApiResponse {
+//    @JSONField(name = "result")
+//    public Object data;
+//
+//    @JSONField(name = "code")
+//    public int code = -1;
+//    //
+//    @JSONField(name = "message")
+//    public String msg;
+//
+//    @JSONField(name = "ret")
+//    public int ret;
+
+    public Object getData() {
+        return this;
+    }
+
+//    public int getCode() {
+//        return code;
+//    }
+
+//    public String getResult() {
+//        return msg;
+//    }
+
+    public boolean isRequestSuccess() {
+        return true;
+    }
+}
